5d47b746183c037fedc7709a560c7448e5ad22fc
stelios <stelios05@gmail.com>
2020-02-05 20:29:52 +0000

    deploys to ossrh snapshot

---
+++ DEPLOY.md
@@ -0,0 +1,2 @@
+#deploy to maven
+see [maven](https://central.sonatype.org/pages/ossrh-guide.html)
---
+++ src/main/java/io/github/steliospaps/dataloader/Dummy.java
@@ -0,0 +1,10 @@
+package io.github.steliospaps.dataloader;
+
+/**
+ * dummy class to allow something to be deployed to oss.sonatype.org for the first time
+ * @author stelios
+ *
+ */
+public class Dummy {
+
+}
